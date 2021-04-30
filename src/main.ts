f5053f4e91844055c0ecd2d27c825a27a622b0ca
Robert <constantinescu.robert94@gmail.com>
2021-04-30 13:29:29 +0300

    -added functionality to strip away properties that should not exist in payload - added functionality to throw an error if the payload contains unwanted properties

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,20 @@ import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+      new ValidationPipe({
+        /**
+         * whitelist: true => is used to avoid letting clients passing invalid properties to the controllers POST request
+         *                    this feature will make sure that the unwanted properties are automatically stripped out and removed
+         *
+         * forbidNonWhitelisted: true => gives us the option to STOP a request if any non white listed properties are present
+         *                              throwing an errors instead
+         */
+        whitelist: true,
+        forbidNonWhitelisted: true
+
+      })
+  );
   await app.listen(3000);
 }
 bootstrap();
