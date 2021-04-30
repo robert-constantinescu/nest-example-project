6d50c33580b09b66cbb40c21fcb7cd0058a41827
Robert <constantinescu.robert94@gmail.com>
2021-04-30 13:04:59 +0300

    making the update dto class properties optional

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,6 +1,7 @@
 export class UpdateCoffeeDto {
     // the readonly keyword helps maintain immutability for the CreateCoffeeDto objects
-    readonly name: string;
-    readonly brand: string;
-    readonly flavors: string[];
+    //the questions marks here means that the property is optional
+    readonly name?: string;
+    readonly brand?: string;
+    readonly flavors?: string[];
 }
