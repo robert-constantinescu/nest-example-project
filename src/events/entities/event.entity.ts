92f47002c74036c993a9b2e10be9a895bc145b39
Robert <constantinescu.robert94@gmail.com>
2021-05-03 17:57:50 +0300

    - adding @Index() when speed is vital important in retrieving an object from DB

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,5 +1,10 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, Index, PrimaryGeneratedColumn} from "typeorm";
 
+/**
+ * @Index(['type', 'name']):
+ *          This is called composite Index() to annotate the whole entity and not each column
+ */
+@Index(['type', 'name'])
 @Entity()
 export class Event {
 
@@ -9,6 +14,13 @@ export class Event {
     @Column()
     type: string;
 
+    /**
+     * If one of the operation our application will do often is retrieving Events based on the name,
+     * we can add the @Index() annotation to ensure rapidity in find and retrieving the object from database.
+     *
+     * We can use @Index() whenever the speed of retrieving and Object is vital important
+     */
+    @Index()
     @Column()
     name: string;
 
