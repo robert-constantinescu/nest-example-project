e35bacc6da068b4359fb9d8ce52371bc8cb3fb14
Robert <constantinescu.robert94@gmail.com>
2021-05-03 18:56:44 +0300

    - using the factory provider pattern

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {Injectable, Module} from '@nestjs/common';
 import {CoffeesController} from "./coffees.controller";
 import {CoffeesService} from "./coffees.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
@@ -6,9 +6,13 @@ import {Coffee} from "./entities/coffee.entity";
 import {Flavor} from "./entities/flavor.entity";
 import {Event} from "../events/entities/event.entity";
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+@Injectable()
+export class CoffeeBrandsFactory {
+    create() {
+        /* do something */
+        return ['buddy brew', 'lavazza'];
+    }
+}
 
 @Module({
     controllers: [CoffeesController],
@@ -37,17 +41,38 @@ class ProductionConfigService {}
      *       provide: ConfigService,
      *       useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService: ProductionConfigService
      *   },
+     * -----------------------------------------------------------------------------------------------------------------
+     * Factory Providers
+     *  - How the Factory provider is looking:
+     *       @Injectable()
+     *       export class CoffeeBrandsFactory {
+     *           create() {
+     *               // do something
+     *           return ['buddy brew', 'lavazza'];
+     *          }
+     *       }
      *
+     * - How the Factory provider is declared in the module:
+     *
+     *    {
+     *       provide: 'COFFEE_BRANDS',
+     *       // basically we call a method from the CoffeeBrandsFactory, that returns an array of brands that we can
+     *       // use by injecting this provider using the 'COFFEE_BRANDS' token
+     *       useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create()
+     *       inject: [CoffeeBrandsFactory] // this is passed in the useFactory function, to use them how we need
+     *   }
      *
      *
      */
     providers: [
         CoffeesService,
+        CoffeeBrandsFactory,
         {
-            provide: ConfigService,
-            useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService: ProductionConfigService
-        },
-        {provide: 'COFFEE_BRANDS', useValue: ['buddy brew', 'lavazza']}],
+            provide: 'COFFEE_BRANDS',
+            useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+            inject: [CoffeeBrandsFactory]
+        }
+    ],
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     exports: [CoffeesService]
 })
